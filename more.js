ca142fcf6e1b568d24f6ae4963bfb8af03eec33a
tbi <tbi@tbi.com>
2015-09-03 09:09:07 +0200

    more.js hinzugfügt

---
+++ more.js
@@ -0,0 +1,24 @@
+var i = function(){
+	2* Math.random();
+}
+
+if (i>1){
+	console.log('Kopf');
+}
+else {
+	console.log('Zahl');
+}
+var j = function(){
+	var rdm=3*Math.random();
+	if (rdm<=1){
+		console.log('Stein');
+	}
+	else if (rdm>1 && rdm<=2){
+		console.log('Schere');
+	}
+	else {
+		console.log('Papier');
+	}
+}
+
+	
