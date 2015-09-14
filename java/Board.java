fab8001d575c0d9da4e8584a0e78c05191767d79
Tobias <teeschu@gmx.de>
2015-09-14 12:15:56 +0000

    changed Board.java

--- java/Board.java
+++ java/Board.java
@@ -2,17 +2,18 @@ public class Board {
 
 	public void draw (){
 		
-		String input[] = new String[10];
+		String fields[] = new String[10];
 		for (int l=0; l<9;l++){
-			input[l]=String.valueOf(l+1);
+			fields[l]=String.valueOf(l+1);
 		}
 		String values [] = {"|", " ", "-"};
 		int counter = 0;
-		String box=values[0]+values[1]+input[counter]+values[1]+values[0];
+		
 
 		for (int i=0; i<5; i++){
 			if (i%2==0){
 				for (int j=0; j<3; j++){
+					String box=values[0]+values[1]+fields[counter]+values[1]+values[0];
 					if (j==2){
 						System.out.print(box+"\n");
 					}
