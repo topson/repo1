0da22268ddf8cfadae779acdb4223e79d73a27d1
tbi <t.schumacher@mediacluster.de>
2015-09-22 09:51:52 +0200

    Added Game over without a winner

--- java/TTTgo.java
+++ java/TTTgo.java
@@ -3,19 +3,25 @@
 class TTTgo {
 	
 	public TTTgo(){
+		int counter = 0;
 		Board gameBoard = new Board();
 		Renderer render = new Renderer();
 		String [][] board = Renderer.initialize();
 		gameBoard.setBoard(board);
 		gameBoard.getBoard();
 		
-		while (GameLogic.gameOver(Board.currBoard) != true){
+		while (GameLogic.gameOver(Board.currBoard) != true && 9 > counter){
+			
 			int inp = Input.getNextInput();
 			GameLogic.checkMove(Board.currBoard, inp);
 			
 			if (GameLogic.retMessage != 9){
 					render.draw(Board.currBoard, inp, GameLogic.retMessage);
 					GameLogic.gameOver(Board.currBoard);
+			}
+			counter++;
+			if (9 == counter){
+				System.out.println("GAME OVER, nobody wins!");
 			}
 		}
 	}
