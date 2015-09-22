81a45a90c011be3136b468b2ee43167c363295ba
tbi <t.schumacher@mediacluster.de>
2015-09-22 08:58:07 +0200

    Added game ending logic

--- java/GameLogic.java
+++ java/GameLogic.java
@@ -24,8 +24,81 @@ class GameLogic {
 	}
 
 	public static boolean gameOver(String[][] currBoard){
-		
 		boolean over = false;
+		int gg = -1;
+
+		if (2 == retMessage){
+			if (currBoard[1][1] == "X"){
+				if(currBoard[1][0] == "X" && currBoard[1][2] == "X"){
+					gg = 1;
+				}
+				else if(currBoard[0][0] == "X" && currBoard[2][2] == "X"){
+					gg = 1;
+				}
+				else if(currBoard[2][0] == "X" && currBoard[0][2] == "X"){
+					gg = 1;
+				}
+				else if(currBoard[0][1] == "X" && currBoard[2][1] == "X"){
+					gg = 1;
+				}
+			}
+			else if (currBoard[0][0] == "X"){
+				if (currBoard[1][0] == "X" && currBoard[2][0] == "X"){
+					gg = 1;
+				}
+				else if (currBoard[0][1] == "X" && currBoard[0][2] == "X"){
+					gg = 1;
+				}
+			}
+			else if (currBoard[2][2] == "X"){
+				if (currBoard [0][2] == "X" && currBoard[1][2] == "X"){
+					gg = 1;
+				}
+				else if (currBoard[2][0] == "X" && currBoard[2][1] == "X"){
+					gg = 1;
+				}
+			}
+			if (1 == gg){
+				System.out.println("GAME OVER, Player1 wins");
+				over = true;
+			}
+		}
+		else if(3 == retMessage){
+			if (currBoard[1][1] == "O"){
+				if(currBoard[1][0] == "O" && currBoard[1][2] == "O"){
+					gg = 1;
+				}
+				else if(currBoard[0][0] == "O" && currBoard[2][2] == "O"){
+					gg = 1;
+				}
+				else if(currBoard[2][0] == "O" && currBoard[0][2] == "O"){
+					gg = 1;
+				}
+				else if(currBoard[0][1] == "O" && currBoard[2][1] == "O"){
+					gg = 1;
+				}
+			}
+			else if (currBoard[0][0] == "O"){
+				if (currBoard[1][0] == "O" && currBoard[2][0] == "O"){
+					gg = 1;
+				}
+				else if (currBoard[0][1] == "O" && currBoard[0][2] == "O"){
+					gg = 1;
+				}
+			}
+			else if (currBoard[2][2] == "O"){
+				if (currBoard [0][2] == "O" && currBoard[1][2] == "O"){
+					gg = 1;
+				}
+				else if (currBoard[2][0] == "O" && currBoard[2][1] == "O"){
+					gg = 1;
+				}
+			}
+			if(1 == gg){
+				System.out.println("GAME OVER, Player2 wins");
+				over = true;
+			}
+		}
 		return over;
 	}
 		
